5a5e211bddf6f759c7ba87faea887fc49ab360fc
devbotas <simon@devbot.lt>
2024-12-04 10:10:08 +0200

    Reformatting code.

--- Tech.Tevux.Dashboards.Controls.Common/Code/AppearanceRule.cs
+++ Tech.Tevux.Dashboards.Controls.Common/Code/AppearanceRule.cs
@@ -25,55 +25,6 @@ public class AppearanceRule : IAppearanceRule {
         Style = AppearanceRuleStyle.FromType(type);
     }
 
-    /// <inheritdoc/>
-    public AppearanceRuleCondition Condition { get; set; }
-
-    /// <inheritdoc/>
-    public IAppearanceRuleStyle Style { get; set; }
-
-    /// <inheritdoc/>
-    public string TextFormat { get; set; }
-
-    /// <inheritdoc/>
-    public string Value {
-        get {
-            return _stringValue;
-        }
-        set {
-            if (decimal.TryParse(value, NumberStyles.Number, CultureInfo.InvariantCulture, out var number)) {
-                _stringValue = value;
-                _decimalValue = number;
-            }
-        }
-    }
-
-    /// <summary>
-    /// Tries parsing a rule from a string.
-    /// </summary>
-    public static bool TryParse(string rawString, out AppearanceRule rule) {
-        if (rawString is null) { goto error;}
-        
-        var ruleParts = rawString.Split('|');
-
-        if (ruleParts.Length < 3) { goto error; }
-
-        if (TryParseCondition(ruleParts[0], out var condition) == false) { goto error; }
-        if (TryParseType(ruleParts[2], out var type) == false) { goto error; }
-
-        var format = "";
-        if (ruleParts.Length > 3) {
-            format = ruleParts[3];
-        }
-
-        rule = new AppearanceRule(condition, ruleParts[1], type, format);
-
-        return true;
-
-    error:
-        rule = new AppearanceRule();
-        return false;
-    }
-
     /// <summary>
     /// Checks if a value matches any conditions in the rule list.
     /// </summary>
@@ -96,6 +47,80 @@ public class AppearanceRule : IAppearanceRule {
 
         return returnString;
     }
+
+    /// <summary>
+    /// Tries parsing a rule from a string.
+    /// </summary>
+    public static bool TryParse(string rawString, out AppearanceRule rule) {
+        if (rawString is null) { goto error; }
+
+        var ruleParts = rawString.Split('|');
+
+        if (ruleParts.Length < 3) { goto error; }
+
+        if (TryParseCondition(ruleParts[0], out var condition) == false) { goto error; }
+        if (TryParseType(ruleParts[2], out var type) == false) { goto error; }
+
+        var format = "";
+        if (ruleParts.Length > 3) {
+            format = ruleParts[3];
+        }
+
+        rule = new AppearanceRule(condition, ruleParts[1], type, format);
+
+        return true;
+
+        error:
+        rule = new AppearanceRule();
+        return false;
+    }
+
+    private bool Matches(string x, string y) {
+        switch (Condition) {
+            case AppearanceRuleCondition.Equal:
+                return x == y;
+
+            case AppearanceRuleCondition.NotEqual:
+                return x != y;
+
+            default:
+                return false;
+        }
+    }
+
+    private bool Matches(decimal x, decimal? y) {
+        if (y.HasValue == false) { return false; }
+
+        switch (Condition) {
+            case AppearanceRuleCondition.Equal:
+                return x == y;
+
+            case AppearanceRuleCondition.NotEqual:
+                return x != y;
+
+            case AppearanceRuleCondition.LessThan:
+                return x < y;
+
+            case AppearanceRuleCondition.LessThanOrEqual:
+                return x <= y;
+
+            case AppearanceRuleCondition.MoreThan:
+                return x > y;
+
+            case AppearanceRuleCondition.MoreThanOrEqual:
+                return x >= y;
+
+            case AppearanceRuleCondition.BitSet:
+                return ((((int)(x) >> (int)(y)) & 1) == 1);
+
+            case AppearanceRuleCondition.BitNotSet:
+                return ((((int)(x) >> (int)(y)) & 1) == 0);
+
+            default:
+                return false;
+        }
+    }
+
     private static string ShortenCondition(AppearanceRuleCondition condition) {
         switch (condition) {
             case AppearanceRuleCondition.Equal:
@@ -216,49 +241,30 @@ public class AppearanceRule : IAppearanceRule {
 
         return returnValue;
     }
-    private bool Matches(string x, string y) {
-        switch (Condition) {
-            case AppearanceRuleCondition.Equal:
-                return x == y;
 
-            case AppearanceRuleCondition.NotEqual:
-                return x != y;
+    #region IAppearanceRule Members
 
-            default:
-                return false;
+    /// <inheritdoc/>
+    public AppearanceRuleCondition Condition { get; set; }
+
+    /// <inheritdoc/>
+    public IAppearanceRuleStyle Style { get; set; }
+
+    /// <inheritdoc/>
+    public string TextFormat { get; set; }
+
+    /// <inheritdoc/>
+    public string Value {
+        get {
+            return _stringValue;
+        }
+        set {
+            if (decimal.TryParse(value, NumberStyles.Number, CultureInfo.InvariantCulture, out var number)) {
+                _stringValue = value;
+                _decimalValue = number;
+            }
         }
     }
 
-    private bool Matches(decimal x, decimal? y) {
-        if (y.HasValue == false) { return false; }
-
-        switch (Condition) {
-            case AppearanceRuleCondition.Equal:
-                return x == y;
-
-            case AppearanceRuleCondition.NotEqual:
-                return x != y;
-
-            case AppearanceRuleCondition.LessThan:
-                return x < y;
-
-            case AppearanceRuleCondition.LessThanOrEqual:
-                return x <= y;
-
-            case AppearanceRuleCondition.MoreThan:
-                return x > y;
-
-            case AppearanceRuleCondition.MoreThanOrEqual:
-                return x >= y;
-
-            case AppearanceRuleCondition.BitSet:
-                return ((((int)(x) >> (int)(y)) & 1) == 1);
-
-            case AppearanceRuleCondition.BitNotSet:
-                return ((((int)(x) >> (int)(y)) & 1) == 0);
-
-            default:
-                return false;
-        }
-    }
+    #endregion
 }
